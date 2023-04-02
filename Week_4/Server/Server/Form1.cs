149f07ade0598d753c95bc62ec69dc73088453ab
pokilee10 <21522286@gm.uit.edu.vn>
2023-04-02 13:29:33 +0700

    Thêm chức năng Connect với Client

--- Week_4/Server/Server/Form1.cs
+++ Week_4/Server/Server/Form1.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
+using System.Threading;
 
 namespace Server
 {
     public partial class Form1 : Form
     {
+        IPEndPoint IP;
+        Socket server;
+        List<Socket> clientList;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Send()
+        {
+            
+        }
+
+        void Connect()
+        {
+            clientList = new List<Socket>();
+            IP = new IPEndPoint(IPAddress.Any, 0);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(IP);
+            Thread Listen = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        server.Listen(100);
+                        Socket client = server.Accept();
+                        clientList.Add(client);
+
+                        Thread receive = new Thread(Receive);
+                        receive.IsBackground = true;
+                        receive.Start(client);
+                    }
+                }
+                catch
+                {
+                    IP = new IPEndPoint(IPAddress.Any, 0);
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                }
+            });
+            Listen.IsBackground = true;
+            Listen.Start();
         }
     }
 }

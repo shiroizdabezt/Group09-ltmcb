46580bbcbb25eb13d2ae18712938efb2dd74738f
SleepySouls <128994152+SleepySouls@users.noreply.github.com>
2023-04-20 21:42:30 +0700

    Add files via upload
    GET function

---
+++ Week 6/Week6/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Week6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_get = new System.Windows.Forms.Button();
+            this.dgv_display = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt_get
+            // 
+            this.bt_get.Location = new System.Drawing.Point(622, 87);
+            this.bt_get.Name = "bt_get";
+            this.bt_get.Size = new System.Drawing.Size(149, 251);
+            this.bt_get.TabIndex = 7;
+            this.bt_get.Text = "Get API";
+            this.bt_get.UseVisualStyleBackColor = true;
+            this.bt_get.Click += new System.EventHandler(this.bt_get_Click);
+            // 
+            // dgv_display
+            // 
+            this.dgv_display.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_display.Location = new System.Drawing.Point(12, 2);
+            this.dgv_display.Name = "dgv_display";
+            this.dgv_display.RowHeadersWidth = 51;
+            this.dgv_display.RowTemplate.Height = 24;
+            this.dgv_display.Size = new System.Drawing.Size(572, 446);
+            this.dgv_display.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_get);
+            this.Controls.Add(this.dgv_display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_display)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bt_get;
+        private System.Windows.Forms.DataGridView dgv_display;
+    }
+}
+
---
+++ Week 6/Week6/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class TodoTask
+        {
+            public int postID { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string body { get; set; }
+  
+        }
+        public static HttpClient client = new HttpClient();
+
+        private void DisplayTodoList(List<TodoTask> task)
+        {
+            dgv_display.DataSource = null;
+            dgv_display.Rows.Clear();
+            dgv_display.DataSource = task;
+        }
+        private async void GetTasksAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/comments");
+
+            if (response.IsSuccessStatusCode)
+            {
+                List<TodoTask> task = await response.Content.ReadAsAsync<List<TodoTask>>();
+                DisplayTodoList(task);
+                MessageBox.Show("Successful!");
+            }
+            else
+                MessageBox.Show("Fail!");
+        }
+        private void bt_get_Click(object sender, EventArgs e)
+        {
+            GetTasksAsync();
+        }
+    }
+}

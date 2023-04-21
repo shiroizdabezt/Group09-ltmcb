8dba06f610482836e171f2a71b9721c85cfe7536
lexuanlinh <troilanhnuc@gmail.com>
2023-04-21 21:32:41 +0700

    add link API (albums and comments)

--- Week 6/Week6/Form1.Designer.cs
+++ Week 6/Week6/Form1.Designer.cs
@@ -30,14 +30,17 @@
         {
             this.bt_get = new System.Windows.Forms.Button();
             this.dgv_display = new System.Windows.Forms.DataGridView();
+            this.cbbAPI = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_display)).BeginInit();
             this.SuspendLayout();
             // 
             // bt_get
             // 
-            this.bt_get.Location = new System.Drawing.Point(622, 87);
+            this.bt_get.Location = new System.Drawing.Point(756, 12);
+            this.bt_get.Margin = new System.Windows.Forms.Padding(2);
             this.bt_get.Name = "bt_get";
-            this.bt_get.Size = new System.Drawing.Size(149, 251);
+            this.bt_get.Size = new System.Drawing.Size(112, 26);
             this.bt_get.TabIndex = 7;
             this.bt_get.Text = "Get API";
             this.bt_get.UseVisualStyleBackColor = true;
@@ -45,31 +48,62 @@
             // 
             // dgv_display
             // 
+            this.dgv_display.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgv_display.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_display.Location = new System.Drawing.Point(12, 2);
+            this.dgv_display.Location = new System.Drawing.Point(11, 42);
+            this.dgv_display.Margin = new System.Windows.Forms.Padding(2);
             this.dgv_display.Name = "dgv_display";
             this.dgv_display.RowHeadersWidth = 51;
             this.dgv_display.RowTemplate.Height = 24;
-            this.dgv_display.Size = new System.Drawing.Size(572, 446);
+            this.dgv_display.Size = new System.Drawing.Size(859, 555);
             this.dgv_display.TabIndex = 5;
+            // 
+            // cbbAPI
+            // 
+            this.cbbAPI.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbAPI.FormattingEnabled = true;
+            this.cbbAPI.Items.AddRange(new object[] {
+            "https://jsonplaceholder.typicode.com/todos",
+            "https://jsonplaceholder.typicode.com/comments",
+            "https://jsonplaceholder.typicode.com/albums"});
+            this.cbbAPI.Location = new System.Drawing.Point(148, 12);
+            this.cbbAPI.Name = "cbbAPI";
+            this.cbbAPI.Size = new System.Drawing.Size(603, 24);
+            this.cbbAPI.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Chose API link:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(879, 608);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbbAPI);
             this.Controls.Add(this.bt_get);
             this.Controls.Add(this.dgv_display);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgv_display)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button bt_get;
         private System.Windows.Forms.DataGridView dgv_display;
+        private System.Windows.Forms.ComboBox cbbAPI;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Week 6/Week6/Form1.cs
+++ Week 6/Week6/Form1.cs
@@ -25,6 +25,21 @@ namespace Week6
             public bool completed { get; set; }
            
         }
+        public class Comments
+        {
+            public int postID { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string body { get; set; }
+        }
+        public class Albums
+        {
+            public int userID { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+        }
+
         public static HttpClient client = new HttpClient();
 
         private void DisplayTodoList(List<TodoTask> task)
@@ -33,18 +48,57 @@ namespace Week6
             dgv_display.Rows.Clear();
             dgv_display.DataSource = task;
         }
+
+        private void DisplayComments(List<Comments> task)
+        {
+            dgv_display.DataSource = null;
+            dgv_display.Rows.Clear();
+            dgv_display.DataSource = task;
+        }
+        private void DisplayAlbums(List<Albums> task)
+        {
+            dgv_display.DataSource = null;
+            dgv_display.Rows.Clear();
+            dgv_display.DataSource = task;
+        }
+
         private async void GetTasksAsync()
         {
-            HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos");
-
-            if (response.IsSuccessStatusCode)
+            HttpResponseMessage response = await client.GetAsync(cbbAPI.Text);
+            switch (cbbAPI.Text)
             {
-                List<TodoTask> task = await response.Content.ReadAsAsync<List<TodoTask>>();
-                DisplayTodoList(task);
-                MessageBox.Show("Successful!");
-            }
-            else
-                MessageBox.Show("Fail!");
+                case "https://jsonplaceholder.typicode.com/todos":
+                    if (response.IsSuccessStatusCode)
+                    {
+                        List<TodoTask> task = await response.Content.ReadAsAsync<List<TodoTask>>();
+                        DisplayTodoList(task);
+                        MessageBox.Show("Successful!");
+                    }
+                    else
+                        MessageBox.Show("Fail!");
+                    break;
+                case "https://jsonplaceholder.typicode.com/comments":
+                    if (response.IsSuccessStatusCode)
+                    {
+                        List<Comments> task = await response.Content.ReadAsAsync<List<Comments>>();
+                        DisplayComments(task);
+                        MessageBox.Show("Successful!");
+                    }
+                    else
+                        MessageBox.Show("Fail!");
+                    break;
+                case "https://jsonplaceholder.typicode.com/albums":
+                    if (response.IsSuccessStatusCode)
+                    {
+                        List<Albums> task = await response.Content.ReadAsAsync<List<Albums>>();
+                        DisplayAlbums(task);
+                        MessageBox.Show("Successful!");
+                    }
+                    else
+                        MessageBox.Show("Fail!");
+                    break;
+            }    
+            
         }
         private void bt_get_Click(object sender, EventArgs e)
         {
